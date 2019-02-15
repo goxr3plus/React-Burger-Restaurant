a696ff88297965675dc2196adb37727ca338c4b5
Kentros Alexandros <Alexandros.KENTROS@intrasoft-intl.com>
2019-02-15 12:22:55 +0200

    Outputing Burger Ingredients Dynamically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,16 +1,26 @@
-import React from 'react';
-import './Burger.css';
-import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+import React from "react"
+import "./Burger.css"
+import BurgerIngredient from "./BurgerIngredient/BurgerIngredient"
 
-const burger = () => {
-    return (
-        <div className={"Burger"}>
-         <BurgerIngredient type="bread-top"></BurgerIngredient>   
-         <BurgerIngredient type="cheese"></BurgerIngredient>   
-         <BurgerIngredient type="meat"></BurgerIngredient>   
-         <BurgerIngredient type="bread-bottom"></BurgerIngredient>   
-        </div>
-    );
-};
+const burger = props => {
+   const transformedIngredients = Object.keys(props.ingredients)
+      .map(igKey => {
+         console.log("Ig Key :" + igKey)
+         return [...Array(props.ingredients[igKey])].map((_, i) => {
+            console.log("Ig Key +i :" + igKey + i)
+            return <BurgerIngredient key={igKey + i} type={igKey} />
+         })
+      })
+      .reduce((arr, el) => {
+         return arr.concat(el)
+      })
+   return (
+      <div className={"Burger"}>
+         <BurgerIngredient type="bread-top" />
+         {transformedIngredients}
+         <BurgerIngredient type="bread-bottom" />
+      </div>
+   )
+}
 
-export default burger;
+export default burger
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,12 +1,20 @@
 import React, { Component } from "react";
-import Burger from './../../components/Burger/Burger';
+import Burger from "./../../components/Burger/Burger";
 
 class BurgerBuilder extends Component {
+  state = {
+    ingredients: {
+      salad: 1,
+      bacon: 1,
+      cheese: 2,
+      meat: 2
+    }
+  };
+
   render() {
     return (
       <>
-        <p> Hello </p>
-        <Burger></Burger>
+        <Burger ingredients={this.state.ingredients}/>
         <div> Build Controls 2</div>
       </>
     );
