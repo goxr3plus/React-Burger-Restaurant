9f35a8ae4b6c074260cf0183914366c129530eba
Kentros Alexandros <Alexandros.KENTROS@intrasoft-intl.com>
2019-02-15 16:53:26 +0200

    Added the Price to the Order Summary

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -14,9 +14,16 @@ const orderSummary = props => {
          <h3> Your Order</h3>
          <p> A delicious burger with the following ingredients:</p>
          <ul>{ingredientSummary}</ul>
+         <p>
+            <strong>Total Price:{props.totalPrice}</strong>
+         </p>
          <p>Continue to Checkout?</p>
-         <Button btnType="Danger" clicked={props.cancelPurchaseHandler}>Cancel</Button>
-         <Button btnType="Success" clicked={props.continuePurchaseHandler}>Continue</Button>
+         <Button btnType="Danger" clicked={props.cancelPurchaseHandler}>
+            Cancel
+         </Button>
+         <Button btnType="Success" clicked={props.continuePurchaseHandler}>
+            Continue
+         </Button>
       </>
    )
 }
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -81,8 +81,8 @@ class BurgerBuilder extends Component {
 
       return (
          <>
-            <Modal show={this.state.purchasing} purchaseCancelHandler={this.cancelPurchaseHandler}>
-               <OrderSummary ingredients={this.state.ingredients} cancelPurchaseHandler={this.cancelPurchaseHandler} continuePurchaseHandler={this.continuePurchaseHandler}/>
+            <Modal show={this.state.purchasing} cancelPurchaseHandler={this.cancelPurchaseHandler}>
+               <OrderSummary totalPrice={this.state.totalPrice.toFixed(2)} ingredients={this.state.ingredients} cancelPurchaseHandler={this.cancelPurchaseHandler} continuePurchaseHandler={this.continuePurchaseHandler}/>
             </Modal>
             <Burger ingredients={this.state.ingredients} />
             <BuildControls
