a5e9e09bf25d9107446755099c0932a4e4340bc8
Kentros Alexandros <Alexandros.KENTROS@intrasoft-intl.com>
2019-02-18 17:09:48 +0200

    Updates

---
+++ precache-manifest.3dcd560d70a6327fed5494cc6125b39e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1c1f31672b2a05881275",
+    "url": "/React-Burger-Restaurant/static/css/main.ddd323b7.chunk.css"
+  },
+  {
+    "revision": "1c1f31672b2a05881275",
+    "url": "/React-Burger-Restaurant/static/js/main.1c1f3167.chunk.js"
+  },
+  {
+    "revision": "d68e2fe66762e19b87ee",
+    "url": "/React-Burger-Restaurant/static/js/runtime~main.d68e2fe6.js"
+  },
+  {
+    "revision": "aa95edab04904c1da160",
+    "url": "/React-Burger-Restaurant/static/js/2.aa95edab.chunk.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/React-Burger-Restaurant/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "9e2ba785d3a9af8ad81243db2c145f97",
+    "url": "/React-Burger-Restaurant/index.html"
+  }
+];
