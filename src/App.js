a760f53646fb13efe207e321681d287e35e276d9
Kentros Alexandros <Alexandros.KENTROS@intrasoft-intl.com>
2019-02-21 15:27:02 +0200

    Fixes for Github

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import React, { Component } from "react";
-import { Route } from "react-router-dom";
-import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
-import Checkout from "./containers/Checkout/Checkout";
-import Layout from "./hoc/Layout/Layout";
+import React, { Component } from "react"
+import { Route, Redirect, Switch } from "react-router-dom"
+import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder"
+import Checkout from "./containers/Checkout/Checkout"
+import Layout from "./hoc/Layout/Layout"
 
 class App extends Component {
    render() {
       return (
          <Layout>
-            <Route path="/" exact component={BurgerBuilder} />
-            <Route path="/checkout" component={Checkout} />
+            <Switch>
+               <Route path="/checkout" component={Checkout} />
+               <Route path="/React-Burger-Restaurant" component={BurgerBuilder} />
+               <Redirect from="/" to="/React-Burger-Restaurant" />
+            </Switch>
          </Layout>
       )
    }
