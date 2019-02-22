2ac054f8a5c4e46cdd7b1c34ebc5b1f15d344b72
Kentros Alexandros <crAlexcomp@gmail.com>
2019-02-22 15:06:27 +0200

    Updates

---
+++ precache-manifest.a923789e98c85e828102d56a62cc2f33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "38fd1906fa7151c05393",
+    "url": "/React-Burger-Restaurant/static/css/main.057c6514.chunk.css"
+  },
+  {
+    "revision": "38fd1906fa7151c05393",
+    "url": "/React-Burger-Restaurant/static/js/main.38fd1906.chunk.js"
+  },
+  {
+    "revision": "d68e2fe66762e19b87ee",
+    "url": "/React-Burger-Restaurant/static/js/runtime~main.d68e2fe6.js"
+  },
+  {
+    "revision": "0b948bd68183808d5596",
+    "url": "/React-Burger-Restaurant/static/js/2.0b948bd6.chunk.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/React-Burger-Restaurant/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "aa2a28ff57e8cc802912486fb73bef85",
+    "url": "/React-Burger-Restaurant/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/React-Burger-Restaurant/precache-manifest.9bab7be599f368d1f838804910a18139.js"
+  "/React-Burger-Restaurant/precache-manifest.a923789e98c85e828102d56a62cc2f33.js"
 );
 
 workbox.clientsClaim();
