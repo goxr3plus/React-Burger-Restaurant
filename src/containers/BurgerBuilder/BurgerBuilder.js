c616cc97e28b5f09b8f1ff862dec5b717a05183d
Kentros Alexandros <crAlexcomp@gmail.com>
2019-02-25 17:06:16 +0200

    Connecting Burger Builder Container to our Store

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -6,6 +6,8 @@ import Burger from "./../../components/Burger/Burger"
 import Modal from "./../../components/UI/Modal/Modal"
 import Spinner from "./../../components/UI/Spinner/Spinner"
 import withErrorHandler from "./../../hoc/withErrorHandler/withErrorHandler"
+import { connect } from "react-redux"
+import * as actionTypes from "../../store/actions"
 
 import qs from "qs"
 
@@ -18,7 +20,6 @@ const INGREDIENT_PRICES = {
 
 class BurgerBuilder extends Component {
    state = {
-      ingredients: [],
       totalPrice: 4,
       purchasable: false,
       purchasing: false,
@@ -53,7 +54,7 @@ class BurgerBuilder extends Component {
    }
 
    addIngredientHandler = ingredientType => {
-      const updatedIngredients = this.state.ingredients
+      const updatedIngredients = this.props.ingredients
       updatedIngredients.push(ingredientType)
 
       /* Calculate the New Price */
@@ -62,21 +63,21 @@ class BurgerBuilder extends Component {
       this.updatePurchaseState(updatedIngredients)
    }
 
-   removeIngredientHandler = ingredientType => {
-      const updatedIngredients = this.state.ingredients
-      let lastIndex = updatedIngredients.lastIndexOf(ingredientType)
+   // removeIngredientHandler = ingredientType => {
+   //    const updatedIngredients = this.props.ingredients
+   //    let lastIndex = updatedIngredients.lastIndexOf(ingredientType)
 
-      // Enter if there are ingredients of this type
-      if (lastIndex !== -1) {
-         // Remove the last ingredient matching this type
-         updatedIngredients.splice(lastIndex, 1)
+   //    // Enter if there are ingredients of this type
+   //    if (lastIndex !== -1) {
+   //       // Remove the last ingredient matching this type
+   //       updatedIngredients.splice(lastIndex, 1)
 
-         /* Calculate the New Price */
-         const newPrice = this.state.totalPrice - INGREDIENT_PRICES[ingredientType]
-         this.setState({ totalPrice: newPrice, ingredients: updatedIngredients })
-         this.updatePurchaseState(updatedIngredients)
-      }
-   }
+   //       /* Calculate the New Price */
+   //       const newPrice = this.state.totalPrice - INGREDIENT_PRICES[ingredientType]
+   //       this.setState({ totalPrice: newPrice, ingredients: updatedIngredients })
+   //       this.updatePurchaseState(updatedIngredients)
+   //    }
+   // }
 
    purchaseHandler = () => {
       this.setState({ purchasing: true })
@@ -87,9 +88,8 @@ class BurgerBuilder extends Component {
    }
 
    continuePurchaseHandler = () => {
- 
       //const queryParams = []
-      const queryParams = qs.stringify(this.state.ingredients);
+      const queryParams = qs.stringify(this.props.ingredients)
       //queryParams.push(encodeURIComponent("ingedients") + "=" + encodeURIComponent(this.state.ingredients))
       // for (let i in this.state.ingredients) {
       //    queryParams.push(encodeURIComponent(i) + "=" + encodeURIComponent(this.state.ingredients[i]))
@@ -97,28 +97,28 @@ class BurgerBuilder extends Component {
 
       this.props.history.push({
          pathname: "/checkout",
-         search: "?" + queryParams+":"+this.state.totalPrice.toFixed(2), //.join("&"),
+         search: "?" + queryParams + ":" + this.state.totalPrice.toFixed(2), //.join("&"),
       })
    }
 
    render() {
       const disabledInfo = {
-         ...this.state.ingredients,
+         ...this.props.ingredients,
       }
 
       for (let key in disabledInfo) disabledInfo[key] = disabledInfo[key] <= 0
 
       let orderSummary = null
       let burger = this.state.error ? <p style={{ textAlign: "center" }}> Ingredients can't be loaded </p> : <Spinner />
-      if (this.state.ingredients !== null) {
+      if (this.props.ingredients !== null) {
          // Burger
          burger = (
             <>
-               <Burger ingredients={this.state.ingredients} />
+               <Burger ingredients={this.props.ingredients} />
                <BuildControls
                   disabledInfo={disabledInfo}
-                  add={this.addIngredientHandler}
-                  remove={this.removeIngredientHandler}
+                  add={this.props.addIngredient}
+                  remove={this.props.removeIngredient}
                   totalPrice={this.state.totalPrice}
                   purchasable={this.state.purchasable}
                   purchaseHandler={this.purchaseHandler}
@@ -132,7 +132,7 @@ class BurgerBuilder extends Component {
          ) : (
             <OrderSummary
                totalPrice={this.state.totalPrice.toFixed(2)}
-               ingredients={this.state.ingredients}
+               ingredients={this.props.ingredients}
                cancelPurchaseHandler={this.cancelPurchaseHandler}
                continuePurchaseHandler={this.continuePurchaseHandler}
             />
@@ -152,4 +152,19 @@ class BurgerBuilder extends Component {
    }
 }
 
-export default withErrorHandler(BurgerBuilder, axiosInstance)
+const mapStateToProps = state => {
+   return { ingredients: state.ingredients }
+}
+
+const mapDispatchToProps = dispatch => {
+   return {
+      addIngredient: ingredientName => dispatch({ type: actionTypes.ADD_INGREDIENT, ingredientName: ingredientName }),
+      removeIngredient: ingredientName =>
+         dispatch({ type: actionTypes.REMOVE_INGREDIENT, ingredientName: ingredientName }),
+   }
+}
+
+export default connect(
+   mapStateToProps,
+   mapDispatchToProps
+)(withErrorHandler(BurgerBuilder, axiosInstance))
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,16 +1,41 @@
 import * as actionTypes from "./actions"
 
 const initialState = {
-   ingredients: [],
+   ingredients: ["salad", "cheese", "bacon"],
    totalPrice: 4,
 }
 
 const reducer = (state = initialState, action) => {
-   switch (action.state) {
+   switch (action.type) {
       case actionTypes.ADD_INGREDIENT:
-         return {}
-      case actionTypes.ADD_INGREDIENT:
-         return {}
+         const updatedIngredients1 = state.ingredients.concat(action.ingredientName)
+
+         //  /* Calculate the New Price */
+         //  const newPrice = this.state.totalPrice + INGREDIENT_PRICES[ingredientType]
+         //  this.setState({ totalPrice: newPrice, ingredients: updatedIngredients })
+         //  this.updatePurchaseState(updatedIngredients)
+         return {
+            ...state,
+            ingredients: updatedIngredients1,
+         }
+      case actionTypes.REMOVE_INGREDIENT:
+         const updatedIngredients = [...state.ingredients]
+         let lastIndex = updatedIngredients.lastIndexOf(action.ingredientName)
+
+         // Enter if there are ingredients of this type
+         if (lastIndex !== -1) {
+            // Remove the last ingredient matching this type
+            updatedIngredients.splice(lastIndex, 1)
+
+            /* Calculate the New Price */
+            // const newPrice = this.state.totalPrice - INGREDIENT_PRICES[ingredientType]
+            // this.setState({ totalPrice: newPrice, ingredients: updatedIngredients })
+            // this.updatePurchaseState(updatedIngredients)
+         }
+         return {
+            ...state,
+            ingredients: updatedIngredients,
+         }
       default:
          return state
    }
