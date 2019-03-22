ade4b2ddf59cea5d72fd3671eb121caa73223cd1
Kentros Alexandros <crAlexcomp@gmail.com>
2019-03-22 10:53:46 +0200

    Bug fix for empty burgers in datbase

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -3,21 +3,25 @@ import "./Order.css"
 import Burger from "../Burger/Burger"
 
 const order = props => {
-   const ingredientOutput = props.ingredients.map((ingredient, index) => {
-      return (
-         <span
-            style={{
-               display: "inline-blick",
-               margin: "0 8px",
-               border: "1px solid #ccc",
-               padding: "5px",
-            }}
-            key={index}
-         >
-            {props.ingredients[index]}
-         </span>
-      )
-   })
+   const ingredients = props.ingredients === undefined ? [] : props.ingredients
+   const ingredientOutput =
+      props.ingredients !== undefined
+         ? props.ingredients.map((ingredient, index) => {
+              return (
+                 <span
+                    style={{
+                       display: "inline-blick",
+                       margin: "0 8px",
+                       border: "1px solid #ccc",
+                       padding: "5px",
+                    }}
+                    key={index}
+                 >
+                    {props.ingredients[index]}
+                 </span>
+              )
+           })
+         : "No ingredents ( bugged )"
 
    return (
       <div className="Order">
@@ -25,7 +29,7 @@ const order = props => {
          <p style={{ textAlign: "center" }}>
             <strong>Preview</strong>
          </p>
-         <Burger ingredients={props.ingredients} classy="Burger2" />
+         <Burger ingredients={ingredients} classy="Burger2" />
          <p>
             Price: <strong>USD {props.price}</strong>{" "}
          </p>
