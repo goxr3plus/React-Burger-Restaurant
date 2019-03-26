86e0c829349bb1047cd5020c4b26cffa7bb85df3
Kentros Alexandros <crAlexcomp@gmail.com>
2019-03-26 11:12:32 +0200

    Reducers Refactoring

--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -9,51 +9,72 @@ const initialState = {
 const orderReducer = (state = initialState, action) => {
    switch (action.type) {
       case actionTypes.PURCHASE_INIT:
-         return {
-            ...state,
-            purchased: false,
-         }
+         return purchaseInit(state, action)
       case actionTypes.PURCHASE_BURGER_START:
-         return {
-            ...state,
-            loading: true,
-         }
+         return purchaseBurgerStart(state, action)
       case actionTypes.PURCHASE_BURGER_SUCCESS:
-         console.log("Successful purchase")
-         const newOrder = {
-            ...action.orderData,
-            id: action.orderId,
-         }
-         return {
-            ...state,
-            loading: false,
-            orders: state.orders.concat(newOrder),
-            purchased: true,
-         }
+         return purchaseBurgerSuccess(state, action)
       case actionTypes.PURCHASE_BURGER_FAIL:
-         return {
-            ...state,
-            loading: false,
-         }
+         return purchaseBurgerFail(state, action)
       case actionTypes.FETCH_ORDERS_START:
-         return {
-            ...state,
-            loading: true,
-         }
+         return fetchOrdersStart(state, action)
       case actionTypes.FETCH_ORDERS_FAILED:
-         return {
-            ...state,
-            loading: false,
-         }
+         return fetchOrdersFailed(state, action)
       case actionTypes.FETCH_ORDERS_SUCCESS:
-         return {
-            ...state,
-            orders: action.orders,
-            loading: false,
-         }
-
+         return fetchOrdersSuccess(state, action)
       default:
          return state
+   }
+}
+
+const purchaseInit = (state, action) => {
+   return {
+      ...state,
+      purchased: false,
+   }
+}
+
+const purchaseBurgerStart = (state, action) => {
+   return {
+      ...state,
+      loading: true,
+   }
+}
+const purchaseBurgerSuccess = (state, action) => {
+   const newOrder = {
+      ...action.orderData,
+      id: action.orderId,
+   }
+   return {
+      ...state,
+      loading: false,
+      orders: state.orders.concat(newOrder),
+      purchased: true,
+   }
+}
+const purchaseBurgerFail = (state, action) => {
+   return {
+      ...state,
+      loading: false,
+   }
+}
+const fetchOrdersStart = (state, action) => {
+   return {
+      ...state,
+      loading: true,
+   }
+}
+const fetchOrdersFailed = (state, action) => {
+   return {
+      ...state,
+      loading: false,
+   }
+}
+const fetchOrdersSuccess = (state, action) => {
+   return {
+      ...state,
+      orders: action.orders,
+      loading: false,
    }
 }
 
