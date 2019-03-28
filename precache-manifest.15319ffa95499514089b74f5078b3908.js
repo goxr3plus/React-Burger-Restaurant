39d90f7f5782cf0bdbb076664cf172147f11c5ab
Kentros Alexandros <crAlexcomp@gmail.com>
2019-03-28 15:35:58 +0200

    Updates

--- precache-manifest.15319ffa95499514089b74f5078b3908.js
+++ precache-manifest.15319ffa95499514089b74f5078b3908.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = [
   {
-    "revision": "03739a2fd2c28788f381",
+    "revision": "a774f91e1d21e4ce148f",
     "url": "/React-Burger-Restaurant/static/css/main.4dbca505.chunk.css"
   },
   {
-    "revision": "03739a2fd2c28788f381",
-    "url": "/React-Burger-Restaurant/static/js/main.be505f87.chunk.js"
+    "revision": "a774f91e1d21e4ce148f",
+    "url": "/React-Burger-Restaurant/static/js/main.ba389d49.chunk.js"
   },
   {
     "revision": "6fdc2c030ac58fbae2be",
     "url": "/React-Burger-Restaurant/static/js/runtime~main.9020ab2a.js"
   },
   {
-    "revision": "8d04ceb672bade4fa173",
-    "url": "/React-Burger-Restaurant/static/js/2.ef4e8196.chunk.js"
+    "revision": "c529ee7b0ba196ffcf5f",
+    "url": "/React-Burger-Restaurant/static/js/2.9c3877cd.chunk.js"
   },
   {
     "revision": "b8503d262bffbfb7c67fd6762963e7d1",
     "url": "/React-Burger-Restaurant/static/media/burger-logo.b8503d26.png"
   },
   {
-    "revision": "90fc8b3a19c898b3c33601c166733db7",
+    "revision": "87e6b990bdb3a2b2215f6109ae087b5b",
     "url": "/React-Burger-Restaurant/index.html"
   }
 ];
