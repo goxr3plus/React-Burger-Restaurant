460c7280dbaf426c25d21bc3a56ddda29715c517
Kentros Alexandros <crAlexcomp@gmail.com>
2019-03-28 15:06:43 +0200

    Close Sidedrawer on click

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -13,7 +13,7 @@ const sideDrawer = props => {
    return (
       <>
          <Backdrop show={props.open} clicked={props.closed} />
-         <div className={attachedClasses.join(" ")}>
+         <div className={attachedClasses.join(" ")} onClick={props.closed}>
             <div className={LogoModule.Logo}>
                <Logo />
             </div>
