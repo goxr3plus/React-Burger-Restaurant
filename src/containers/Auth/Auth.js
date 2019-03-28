4427fcc55e4f55443873fd23094dc3fba92d7960
Kentros Alexandros <crAlexcomp@gmail.com>
2019-03-28 15:31:24 +0200

    Code improvmemts

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -6,6 +6,7 @@ import * as actions from "../../store/actions/index"
 import { connect } from "react-redux"
 import Spinner from "../../components/UI/Spinner/Spinner"
 import { Redirect } from "react-router-dom"
+import { updateObject } from "../../shared/utility"
 
 class Auth extends Component {
    state = {
@@ -81,18 +82,17 @@ class Auth extends Component {
    }
 
    inputChangedHandler = (event, controlName) => {
-      const updatedControls = {
-         ...this.state.controls,
-         [controlName]: {
-            ...this.state.controls[controlName],
+      const updatedControls = updateObject(this.state.controls, {
+         [controlName]: updateObject(this.state.controls[controlName], {
             value: event.target.value,
             valid: this.checkValidity(
                event.target.value,
                this.state.controls[controlName].validation
             ),
             touched: true,
-         },
-      }
+         }),
+      })
+
       this.setState({ controls: updatedControls })
    }
 
